f79a7a0b2870d9fcefe14d20351cd684d9ed31e6
Srujan M Padmashali <srujanmpadmashali@gmail.com>
2025-12-24 18:15:58 +0530

    feat: added procedural tiles

--- map.gd
+++ map.gd
@@ -1,82 +1,81 @@
 extends Node
 
-@onready var tile_layer: TileMapLayer = $TileMap
+@export var width := 800
+@export var height := 800
 
-# MAP SIZE
-const MAP_WIDTH = 100
-const MAP_HEIGHT = 100
+@onready var tilemap : TileMapLayer = $TileMap
+@onready var noise := FastNoiseLite.new()
 
-# ATLAS SOURCE ID
-const ATLAS_ID = 0
+const SRC := 0  # atlas source id
 
-# ATLAS COORDINATES
-const WATER = Vector2i(0, 0)
-const SAND = Vector2i(6, 0)
+const GRASS = Vector2i(0, 0)
+const DIRT  = Vector2i(1, 0)
+const CLAY  = Vector2i(2, 0)
+const MUD   = Vector2i(3, 0)
 
-const LIGHT_GRASS = Vector2i(0, 3)
-const DARK_GRASS = Vector2i(3, 3)
-const MEDIUM_DARK_GRASS = Vector2i(6, 3)
-const BRIGHT_GRASS = Vector2i(0, 6)
-const DULL_GRASS = Vector2i(3, 6)
-
-const DRIED_GRASS = Vector2i(3, 9)
-const DARK_DRIED_GRASS = Vector2i(0, 9)
-
-const MAGMA = Vector2i(6, 6)
+const SAND  = Vector2i(0, 1)
+const LAVA  = Vector2i(1, 1)
+const MAGMA = Vector2i(2, 1)
+const WATER = Vector2i(3, 1)
 
 func _ready():
-	randomize()
-	generate_island()
+	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	noise.frequency = 0.04
+	noise.seed = randi()
 
+	generate_world()
+func fill_water():
+	for x in width:
+		for y in height:
+			tilemap.set_cell(Vector2i(x, y), SRC, WATER)
 func generate_island():
-	tile_layer.clear()
+	for x in width:
+		for y in height:
+			var n := noise.get_noise_2d(x, y)
 
-	var center := Vector2(MAP_WIDTH / 2, MAP_HEIGHT / 2)
-	var max_radius := min(MAP_WIDTH, MAP_HEIGHT) / 2 - 5
+			if n > -0.1:
+				tilemap.set_cell(Vector2i(x, y), SRC, GRASS)
+func add_sand_edges():
+	for x in range(1, width - 1):
+		for y in range(1, height - 1):
+			var pos := Vector2i(x, y)
 
-	for x in MAP_WIDTH:
-		for y in MAP_HEIGHT:
-			var pos := Vector2(x, y)
-			var distance := pos.distance_to(center)
+			if tilemap.get_cell_atlas_coords(pos) == GRASS:
+				for d in [Vector2i.LEFT, Vector2i.RIGHT, Vector2i.UP, Vector2i.DOWN]:
+					if tilemap.get_cell_atlas_coords(pos + d) == WATER:
+						tilemap.set_cell(pos, SRC, SAND)
+						break
+func place_patches(tile: Vector2i, threshold: float):
+	for x in width:
+		for y in height:
+			var n := noise.get_noise_2d(x + randi() % 999, y + randi() % 999)
 
-			# Organic edges
-			var noise := randf_range(-6.0, 6.0)
-			var island_radius := max_radius + noise
+			if n > threshold:
+				var pos := Vector2i(x, y)
+				if tilemap.get_cell_atlas_coords(pos) == GRASS:
+					tilemap.set_cell(pos, SRC, tile)
 
-			if distance > island_radius:
-				set_tile(x, y, WATER)
-			elif distance > island_radius - 4:
-				set_tile(x, y, SAND)
-			else:
-				set_tile(x, y, random_grass())
+func generate_world():
+	fill_water()
+	generate_island()
+	add_sand_edges()
 
-	add_magma_core(center)
+	# Medium dirt clusters
+	place_patches(DIRT, 0.45)
 
-func random_grass() -> Vector2i:
-	var grass_tiles := [
-		LIGHT_GRASS,
-		DARK_GRASS,
-		MEDIUM_DARK_GRASS,
-		BRIGHT_GRASS,
-		DULL_GRASS,
-		DRIED_GRASS,
-		DARK_DRIED_GRASS
-	]
-	return grass_tiles.pick_random()
+	# Small magma clusters
+	place_patches(MAGMA, 0.65)
 
-func add_magma_core(center: Vector2):
-	if randf() < 0.3:
-		for x in range(-3, 4):
-			for y in range(-3, 4):
-				set_tile(
-					int(center.x) + x,
-					int(center.y) + y,
-					MAGMA
-				)
+	# Lava inside magma
+	for x in width:
+		for y in height:
+			var pos := Vector2i(x, y)
+			if tilemap.get_cell_atlas_coords(pos) == MAGMA and randf() < 0.15:
+				tilemap.set_cell(pos, SRC, LAVA)
 
-func set_tile(x: int, y: int, atlas_coords: Vector2i):
-	tile_layer.set_cell(
-		Vector2i(x, y),   # cell position
-		ATLAS_ID,         # atlas source id
-		atlas_coords      # atlas coords
-	)
+	# Small ponds
+	place_patches(WATER, 0.6)
+
+	# Very small patches
+	place_patches(MUD, 0.72)
+	place_patches(CLAY, 0.75)
